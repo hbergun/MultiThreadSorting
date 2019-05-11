ce725273a873ccbde11d0fe90a44f6ed230d540b
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-05-12 01:46:22 +0300

    Sorting Alg Completed

---
+++ MultiThreadSorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiThreadSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ MultiThreadSorting/Sort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiThreadSorting
+{
+    public class Sort
+    {
+        public static void Bubble(int[] myArray)
+        {
+            int i = 1, j, value;
+            int arrayCount = myArray.Length;
+            while (i < arrayCount)
+            {
+                j = arrayCount - 1;
+                while (j >= 1)
+                {
+                    if (myArray[j - 1] > myArray[j])
+                    {
+                        value = myArray[j];
+                        myArray[j] = myArray[j - 1];
+                        myArray[j - 1] = value;
+                    }
+                    j--;
+                }
+                i++;
+            }
+        }
+    }
+}
