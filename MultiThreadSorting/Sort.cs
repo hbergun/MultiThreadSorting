dc93cac03d5fa52982b19bc624eb8a9c65313a2b
Berk Ergün <hasan.berk.ergun@hotmail.com>
2019-05-24 13:42:19 +0300

    Added Sorting Algorithm

--- MultiThreadSorting/Sort.cs
+++ MultiThreadSorting/Sort.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 
@@ -9,6 +10,29 @@ namespace MultiThreadSorting
     {
         public static int[] Bubble(int[] myArray)
         {
+            int i = 1, j, value;
+            int arrayCount = myArray.Length;
+            while (i < arrayCount)
+            {
+                j = arrayCount - 1;
+                while (j >= 1)
+                {
+                    if (myArray[j - 1] > myArray[j])
+                    {
+                        value = myArray[j];
+                        myArray[j] = myArray[j - 1];
+                        myArray[j - 1] = value;
+                    }
+                    j--;
+                }
+                i++;
+            }
+            return myArray;
+        }
+
+        public static int[] SortThreeArray(int[] arr1, int[] arr2, int[] arr3)
+        {
+            int[] myArray = arr1.Concat(arr2).Concat(arr3).ToArray();
             int i = 1, j, value;
             int arrayCount = myArray.Length;
             while (i < arrayCount)
